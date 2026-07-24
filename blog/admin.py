6d781b467dfbfdc1f0e21e7af2bdf53b30e8e324
Narendra Nath Yadav <narendranathyadav67@gmail.com>
2026-07-24 16:35:59 +0530

    Initial commit - Nextify Management Consultant website (converted from Django to plain HTML)

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Blog
-
-admin.site.site_header = "Nextify Admin"
-admin.site.index_title = "Welcome to Nextify Admin Portal"
-admin.site.site_title = "Nextify Management Consultant"
-
-
-admin.site.register(Blog)
--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),  # Include blog URLs
-    path('accounts/', include('accounts.urls')),  # Include accounts URLs
-    path('', include('Home.urls')),
-]
